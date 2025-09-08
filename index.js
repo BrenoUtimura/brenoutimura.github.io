85d871f0c2787184664be47535cc2455e9b923b3
Breno Utimura <breno.hideki.utimura@gmail.com>
2025-09-08 00:15:11 -0300

    Update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+/* declaração do documento do JavaScript e declaração de duas constantes */
 document.addEventListener('DOMContentLoaded', () => {
   const menuBurguer = document.querySelector(".menu-burger");
   const navList = document.querySelector(".nav-list");
+  const navListItem = document.querySelector('.nav-list-item');
 
+  /* Função que chama a animação do menuBurger na página  */
   menuBurguer.addEventListener("click", () => {
     navList.classList.toggle("active");
   });
+
 });
